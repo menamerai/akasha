dcad981f03edf6955a8585fd9e9dd94206db0c13
menamerai <rphananh@gmail.com>
2023-10-14 01:05:22 -0400

    akashic record unit basic commands

--- contracts/Record.sol
+++ contracts/Record.sol
@@ -16,6 +16,8 @@ contract Record {
     string public description;
     uint256 public timestamp;
     mapping(address => mapping(string => string)) public flashcards;
+    mapping(address => string[]) public questions;
+    
 
     constructor(
         string memory _title,
@@ -25,6 +27,10 @@ contract Record {
         title = _title;
         description = _description;
         timestamp = block.timestamp;
+    }
+
+    function getAllQuestions() public view returns (string[] memory) {
+        return questions[msg.sender];
     }
 
     function addFlashcard(string memory _question, string memory _answer) public {
@@ -37,5 +43,36 @@ contract Record {
 
         // add the question and answer to the mapping
         flashcards[msg.sender][_question] = _answer;
+        questions[msg.sender].push(_question);
+    }
+
+    function removeFlashcard(string memory _question) public {
+        // check if the question is already in the mapping
+        require(
+            keccak256(abi.encodePacked(flashcards[msg.sender][_question])) !=
+                keccak256(abi.encodePacked("")),
+            "Question does not exist"
+        );
+
+        // remove the question and answer from the mapping
+        delete flashcards[msg.sender][_question];
+        // remove the question from the questions array
+        for (uint256 i = 0; i < questions[msg.sender].length; i++) {
+            if (
+                keccak256(abi.encodePacked(questions[msg.sender][i])) ==
+                keccak256(abi.encodePacked(_question))
+            ) {
+                delete questions[msg.sender][i];
+            }
+        }
+    }
+
+    function getAllFlashcards() public view returns (string[] memory, string[] memory) {
+        string[] memory _questions = questions[msg.sender];
+        string[] memory _answers = new string[](_questions.length);
+        for (uint256 i = 0; i < _questions.length; i++) {
+            _answers[i] = flashcards[msg.sender][_questions[i]];
+        }
+        return (_questions, _answers);
     }
 }
