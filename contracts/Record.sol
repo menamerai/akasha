6047fbaf94b0867fd586c8fcc5f470373ffd5f7f
menamerai <rphananh@gmail.com>
2023-10-13 23:52:01 -0400

    record scale down

--- contracts/Record.sol
+++ contracts/Record.sol
@@ -4,91 +4,38 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
-// A contract called Record that contains a struct storing information on a certain subject - like a wiki page
-// The struct should also contain a uint that acts as a timestamp.
+// A contract called Record that contains information on a certain subject - like a wiki page
+// it has a title, description, and timestamp
 // There should also include a mapping that maps an address to a question - answer tuple pair
 // representing a flashcard question and answer added by the user
+// try to keep the contract as simple and as gas efficient as possible
 
 contract Record {
     address public owner;
-    struct RecordStruct {
-        string title;
-        string description;
-        uint timestamp;
-    }
+    string public title;
+    string public description;
+    uint256 public timestamp;
+    mapping(address => mapping(string => string)) public flashcards;
 
-    mapping(address => mapping(string => mapping(address => mapping (string => string)))) public flashcards; // address => record title => address => question => answer
-    RecordStruct[] public records;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function addRecord(
+    constructor(
         string memory _title,
-        string memory _description,
-        uint _timestamp
-    ) public {
-        require(msg.sender == owner, "You are not the owner of the contract");
-        records.push(RecordStruct(_title, _description, _timestamp));
+        string memory _description
+    ) {
+        owner = msg.sender;
+        title = _title;
+        description = _description;
+        timestamp = block.timestamp;
     }
 
-    function addFlashcard(
-        string memory _question, 
-        string memory _answer,
-        string memory _title
-    ) public {
-        // check if the record title exists
-        for (uint i = 0; i < records.length; i++) {
-            if (keccak256(abi.encodePacked(records[i].title)) == keccak256(abi.encodePacked(_title))) {
-                // check if the question exists
-                if (keccak256(abi.encodePacked(flashcards[msg.sender][_title][msg.sender][_question])) == keccak256(abi.encodePacked(_question))) {
-                    revert("Question already exists");
-                } else {
-                    flashcards[msg.sender][_title][msg.sender][_question] = _answer;
-                }
-            } else {
-                revert("Record title does not exist");
-            }
-        }
-    }
+    function addFlashcard(string memory _question, string memory _answer) public {
+        // check if the question is already in the mapping
+        require(
+            keccak256(abi.encodePacked(flashcards[msg.sender][_question])) ==
+                keccak256(abi.encodePacked("")),
+            "Question already exists"
+        );
 
-    function getFlashcard(
-        string memory _question,
-        string memory _title
-    ) public view returns (string memory) {
-        return flashcards[msg.sender][_title][msg.sender][_question];
-    }
-
-    function getAllRecordTitles() public view returns (string[] memory) {
-        string[] memory titles = new string[](records.length);
-        for (uint i = 0; i < records.length; i++) {
-            titles[i] = records[i].title;
-        }
-        return titles;
-    }
-
-    function getRecord(uint _index) public view returns (string memory, string memory, uint) {
-        return (records[_index].title, records[_index].description, records[_index].timestamp);
-    }
-
-    function getRecordCount() public view returns (uint) {
-        return records.length;
-    }
-
-    function getAllFlashcardAnswers(string memory _title) public view returns (string[] memory) {
-        string[] memory answers = new string[](records.length);
-        for (uint i = 0; i < records.length; i++) {
-            answers[i] = flashcards[msg.sender][_title][msg.sender][records[i].title];
-        }
-        return answers;
-    }
-
-    function getAllFlashcardQuestions(string memory _title) public view returns (string[] memory) {
-        string[] memory questions = new string[](records.length);
-        for (uint i = 0; i < records.length; i++) {
-            questions[i] = flashcards[msg.sender][_title][msg.sender][records[i].title];
-        }
-        return questions;
+        // add the question and answer to the mapping
+        flashcards[msg.sender][_question] = _answer;
     }
 }
