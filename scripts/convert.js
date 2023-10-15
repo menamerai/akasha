b9b2a88ef4f53c642c5524d4c84ad71b241b0a65
menamerai <rphananh@gmail.com>
2023-10-15 00:31:17 -0400

    add helper js

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,6 +1,6 @@
 const bigInt = require('big-integer');
 
-const hexString = "ae56b2c01e4cbdfb645c3edaa23ea63ff93425c56e41320461f2623d46ff4ecb";
+const hexString = "bce80fd2cd006d3e910c844a8c43ccda335abad5ea59609a054f13687726c32e";
 const integer = bigInt(hexString, 16);
 
 console.log(integer.toString());
